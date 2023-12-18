9dd8806479b8eb8a6d24d15fe5d347faeacb50a3
Viktor <bogdanovvitek@mail.ru>
2023-12-18 23:35:38 +0300

    m c2 commit

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -5,11 +5,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
+
+    private Needle7 needle;
+
     @Autowired
-    private Needle7 needle7;
+    public Egg6(Needle7 neegle) {
+        this.needle = neegle;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + needle7.toString();
+        return ", в яйце иголка " + needle.toString();
     }
 }
--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -6,11 +6,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class Rabbit4 {
 
+    private Duck5 duck;
+
     @Autowired
-    private Duck5 duck5;
+    public void setDuck5(Duck5 duck) {
+        this.duck = duck;
+    }
 
     @Override
     public String toString() {
-        return ", в зайце утка " + duck5.toString();
+        return ", в зайце утка " + duck.toString();
     }
 }
